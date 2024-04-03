074fdf70c5f9cc8ef51f398c65273e8164ddbce8
phillipohwotemu <ohwotemuphillip@gmail.com>
2024-04-03 05:48:05 -0700

    up

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!")
+    fmt.Fprintf(w, "Hello, Web World!")
 }
 
 func main() {
     http.HandleFunc("/", handler)
-    fmt.Println("Starting server at port 8080")
-    http.ListenAndServe(":8080", nil)
+    fmt.Println("Starting server on :8080")
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Printf("Error starting server: %s\n", err)
+    }
 }
 
